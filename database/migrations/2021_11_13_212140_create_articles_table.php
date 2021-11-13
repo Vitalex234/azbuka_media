6122a707fda0a37a51d7f2da52f579fde80221e1
user <vitalex1@mail.ru>
2021-11-14 02:51:17 +0400

    создание миграций и моделей

---
+++ database/migrations/2021_11_13_212140_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->text('short_desc');
+            $table->string('image_thumb');
+            $table->text('full_content');
+            $table->string('source_url');
+            $table->string('alias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
