34efae04502872260ee80302cb0e57c95306619f
user <vitalex1@mail.ru>
2022-02-07 01:24:58 +0400

    add favicon, удалил заглушку при загрузке страницы

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -506,12 +506,12 @@ function galleryItem() {
     galleryItemChange();
 }
 // Preloader
-$(".preloader").fakeLoader({
-    timeToHide:1200, //Time in milliseconds for fakeLoader disappear
-    zIndex:"999",//Default zIndex
-    spinner:"spinner6",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
-    bgColor:"#333333", //Hex, RGB or RGBA colors
-});
+// $(".preloader").fakeLoader({
+//     timeToHide:1200, //Time in milliseconds for fakeLoader disappear
+//     zIndex:"999",//Default zIndex
+//     spinner:"spinner6",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
+//     bgColor:"#333333", //Hex, RGB or RGBA colors
+// });
 
 $(document).ready(function() {
     'use strict'
